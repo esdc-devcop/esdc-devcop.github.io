8dc598b2265ebaeb411069b7e5321c088adffedc
Paquette <josee.n.paquette@hrsdc-rhdcc.gc.ca>
2021-11-30 11:02:41 -0500

    refactored code to fix build error

--- link-check.js
+++ link-check.js
@@ -6,7 +6,10 @@ var markdownLinkCheck = require('markdown-link-check');
 var fs = require("fs");
 var glob = require("glob");
 var path = require("path");
-var chalk = require("chalk");
+//build fails because build failed: Error [ERR_REQUIRE_ESM] require() of ES Module /home/travis/.../node_modules/chalk/source/index.js is not supported.
+//var chalk = require("chalk");
+//refactored to a dynamic import() which is available in all CommonJS modules.
+import chalk from "chalk"; 
 
 
 var files = glob.sync("**/*.md", {ignore: ["node_modules/**/*.md"]})
