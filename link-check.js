b041e3303caa8ac4bd1aa84218e6c76d24699a49
j-paquette <56076749+j-paquette@users.noreply.github.com>
2022-02-07 16:24:37 -0500

    fixes rebase conflicts

--- link-check.js
+++ link-check.js
@@ -2,10 +2,15 @@
 
 'use strict';
 const {testFileLinks} = require('./node_modules/cdts/TestLinks');
-const directories = ["./_data", "./_guides", "./_includes", "./_layouts", "./_recommendations", "./_references"];
+//Validate Urls in these folders
+const directories = ["./_data", "./_guides", "./_includes", "./_layouts", "./_pages", "./_recommendations", "./_references"];
+//Exception lists: Urls listed are not validated
+//Includes partial Urls that need credentials
 const exceptionSyntax = ["https://014gc.sharepoint.com", "https://dialogue/", "https://architecture/", "http://dialogue/", "http://srmis-sigdi-iagent.prv/"];
+//Includes non-HTTPS Urls internal to the dept 
 const exceptionHTTP = ["http://dialogue/", "http://srmis-sigdi-iagent.prv/"];
-const exceptionIntranet = ["https://jade-repos.intra.dev/", "https://web.microsoftstream.com/video/"];
+//Includes Urls that need credentials
+const exceptionIntranet = ["https://jade-repos.intra.dev/", "https://web.microsoftstream.com/", "https://ado.intra.dmz/ProjectCollection/"];
 
 testFileLinks(directories, exceptionSyntax, exceptionHTTP, exceptionIntranet);
 
